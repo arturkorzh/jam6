6c724b91b44c1a22ad43a429aa8ab570f140f7e0
d.zarnytsia <d.zarnytsia@plarium.com>
2024-06-06 18:31:10 +0300

    block types

--- Assets/Scripts/actors/JoinableView.cs
+++ Assets/Scripts/actors/JoinableView.cs
@@ -1,37 +1,59 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum BlockType
+{
+    S,
+    T,
+    L,
+    I,
+    O,
+}
+
+public enum BuffType
+{
+    Red,
+    Orange,
+    Blue,
+    Green,
+    Yellow,
+    Pink,
+}
 
 public class JoinableView : MonoBehaviour
 {
-  public List<JoinableElementView> Elements;
-  
-  private Rigidbody2D _body;
-  private Collider2D _collider;
+    public List<JoinableElementView> Elements;
 
-  private bool _needRemoveBody;
-  
-  private void Awake()
-  {
-    _body = GetComponent<Rigidbody2D>();
-    _collider = GetComponent<Collider2D>();
-  }
+    private Rigidbody2D _body;
+    private Collider2D _collider;
 
-  private void Update()
-  {
-    if (!_needRemoveBody)
-      return;
-    
-    DestroyImmediate(_body);
+    private bool _needRemoveBody;
+    public int BlockType = 1;
+    public int BuffType = 1;
 
-    _needRemoveBody = false;
-  }
+    private void Awake()
+    {
+        _body = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<Collider2D>();
+    }
 
-  public void JoinToPlayer(int layer)
-  {
-    _needRemoveBody = true;
-    gameObject.layer = layer;
-    
-    foreach (var newElement in Elements)
-      newElement.JoinToPlayer();
-  }
+    private void Update()
+    {
+        if (!_needRemoveBody)
+            return;
+
+        DestroyImmediate(_body);
+
+        _needRemoveBody = false;
+    }
+
+    public void JoinToPlayer(int layer)
+    {
+        _needRemoveBody = true;
+        gameObject.layer = layer;
+
+        foreach (var newElement in Elements)
+            newElement.JoinToPlayer();
+    }
 }
