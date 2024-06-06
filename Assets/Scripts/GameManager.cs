c15adb76f26f84992db7ee63232492355fd1d642
d.zarnytsia <d.zarnytsia@plarium.com>
2024-06-06 17:41:55 +0300

    win checker

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,77 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-  //public PlayerView Player;
+    private class Point
+    {
+        public int x { get; set; }
+        public int y { get; set; }
 
-  private void Awake()
-  {
-    
-  }
-  
-  private void Start()
-  {
-    DontDestroyOnLoad(gameObject);
-  }
+        public Point(int X, int Y)
+        {
+            x = X;
+            y = Y;
+        }
+    }
+
+    public List<int[,]> Figures = new()
+    {
+        new int[5, 5]
+        {
+            { 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0 },
+        },
+        new int[5, 5]
+        {
+            { 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0 },
+        },
+    };
+
+    public int CurrentLevel;
+    public const float cellSize = 0.15f;
+
+    public static bool CheckWinCondition(List<JoinableElementView> elements, Vector3 actorPosition)
+    {
+        var tem = elements.Select(x => x.transform.InverseTransformPoint(actorPosition));
+
+        var temp = tem.Select(x => new Point((int)(x.x / cellSize - 1f), (int)(x.y / cellSize - 1f))).ToList();
+        var minX = temp.Min(x => x.x);
+        var minY = temp.Min(x => x.y);
+
+
+        var xSize = temp.Max(x => x.x) - minX;
+        var ySize = temp.Max(x => x.y) - minY;
+
+        var matrix = new int[xSize, ySize];
+
+
+        foreach (var value in temp)
+        {
+            value.x += minX > 0 ? minX : -(minX);
+            value.y += minY > 0 ? minY : -minY;
+        }
+
+        foreach (var vector in temp)
+            matrix[vector.x, vector.y] = 1;
+
+        return true;
+    }
+
+    private void Awake()
+    {
+    }
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
 }
